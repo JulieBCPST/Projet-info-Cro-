e515db5f629cfb1c6ed70695990390759a93a111
JulieBCPST <61023051+JulieBCPST@users.noreply.github.com>
2020-02-25 21:28:49 +0100

    Update Projet Info Croâ.py

--- Projet Info Croâ.py
+++ Projet Info Croâ.py
@@ -1,6 +1,6 @@
 ### IMPORTATION
 from random import *
-import time
+import time # pour la fonction sleep
 
 
 ### FONCTION INITIALISATION
@@ -9,7 +9,6 @@ import time
 #initialiser un tableau
 def InitPlateau(): #crée un tableau vide pouvant être utilisé pour créer les différents plateaux
     tab = [0]*8
-
     #on assigne  aux 64 cartes du plateau
     for i in range (0,8):
         tab[i] = [0]*8
@@ -59,9 +58,30 @@ def CreateTasCarte():
             index += 1 #fais évoluer l'indice
     return tableaucarte # retourne le tableau de carte
 
+## Condition de victoire
+
+
+def IsFinish(plateauPion): # parcourt le plateau de pion et vérifie si toutes les reines sont présentes
+    j1IsHere = False # la reine 1 n'a pas encore été trouvée
+    j2IsHere = False # la reine 2 n'a pas encore été trouvée
+    for col in range (0,len(plateauPion)): # parcourt les colonnes une par une
+        for lig in range (0,len(plateauPion[col])): # parcourt les lignes sur une colonne
+            if(plateauPion[col][lig] == "J1"): # si l'on rencontre la reine 1 sur une des cases
+                j1IsHere = True
+            if(plateauPion[col][lig] == "J2"): # si l'on rencontre la reine 2 sur une des cases
+                j2IsHere = True
+    if(j1IsHere == True and j2IsHere == True): # les deux reines ont été trouvées
+        return ("La bataille se poursuit.")
+    if(j1IsHere == False and j2IsHere == False) :#aucune des reines n'a été trouvée
+        return ("La bataille est terminée, mais les deux camps sont éléminés, personne ne gagne.")
+    if(j1IsHere == False) : # si la reine du joueur 1 n'a pas été trouvée
+        return ("La bataille est terminée, vous avez perdu.")
+    if(j2IsHere == False) : # si la reine du joueur 2 n'a pas été trouvée
+        return ("La bataille est terminée, vous avez gagné.")
+
 ###Fonction d'affichage
 
-
+## Affichage d'un seul plateau
 #Affichage du plateau (fonction générale)
 def AffichePlateau(tab):
     ESPACE = 4 #nombres d'espaces entre les collones
@@ -94,6 +114,28 @@ def AffichageCarte(tabCarte,tabVisi): # tableau des cartes révélées
                 tabResult[i][j]='*'#la carte n'a pas été révélée
     AffichePlateau(tabResult)#on affiche le tableau des cartes révélées
 
+## Affichage d'un plateau avec un caractère modifié
+#affiche une flèche permettant au joueur de savoir par rapport à quel pion est posé la question
+def AffichePionSelectionne(tab,i,j):
+    pion = tab[i][j] # récupère le pion
+    tab[i][j] = "->" + pion # ajoute à la case une flèche pour montrer à l'utilisateur ce qui est sélectionné
+    AffichePlateau(tab) # afficher le tableau avec le pion selectionné
+    tab[i][j] = pion # rétablir la case en enlevant la flèche
+
+
+## Affichage de deux plateaux alternativement
+#fonction alternative à sleep permettant d'afficher alternativement le plateau des pions et celui des cartes
+def AffichePlateauAlterner(typePlateau,plateauPion,plateauCarte,tabVisi):
+    if(typePlateau == -1): # si c'est -1 afficher les pions
+        AffichePlateau(plateauPion)
+    else:# si c'est 1 afficher les cartes
+        AffichageCarte(plateauCarte,tabVisi)
+
+#fonction permettant d'afficher alternativement le plateau des cartes et celui des pions AVEC sleep
+def AffichePlateauAlternerS(plateauPion,plateauCarte,tabVisi):
+    AffichageCarte(plateauCarte,tabVisi) #afficher les cartes
+    time.sleep(2) # attendre n secondes
+    AffichePlateau(plateauPion) #afficher plateau
 
 
 ### Fonction de jeu
@@ -104,6 +146,34 @@ def Choixpion():#demande au joueur s'il veut changer de vision de plateau, jouer
     while rep!=0 or rep!=1 or rep !=2 :#évite que rep prenne une autre valeur que celle demandée
         rep=input("0 = jouer ce pion, 1 = choisir un autre pion, 2 = afficher une autre face du plateau")
     return rep
+
+
+
+# version avec sleep
+def RecherchePionS(plateauPion,plateauCarte,plateauVisible): #parcourt le plateau tant que le joueur ne choisit pas de pion
+    selectionne = False #le joueur n'a toujours pas sséletionné de pion
+    col = 0 #démarre à (0,0)
+    lig = 0
+    while selectionne == False:#tant que le joueur ne sélectionne pas de pion on parcourt le plateau
+        if(col == len(plateauPion)):#si l'on est en bout de ligne
+            lig += 1 #on saute une ligne
+            col = 0 #on revient à une première colonne
+        if(lig == len(plateauPion)) and (col==len(plateauPion)): # si l'on est en (7,7)
+            lig = 0 # on revient à (0,0)
+            col = 0
+        if(plateauPion[col][lig] == "J1" or plateauPion[col][lig] == "g1"):#si la case sur laquelle est arrivée est occupé par un pion allié
+            rep = 2
+            AffichePionSelectionne(plateauPion,col,lig) # affiche au joueur sur quel pion est posé la question
+            while(rep == 2):
+                rep = Choixpion() #demande au joueur s'il veut jouer ce pion
+                if(rep == 0):#si le joueur choisit de jouer ce pion
+                    selectionne = True
+                    col -= 1 #permet de retourner la bonne colonnne à la fin
+                if(rep == 2): # affiche le plateau des cartes et des pions alternativement si le joueur le souhaite
+                    AffichePlateauAlternerS(plateauPion,plateauCarte,plateauVisible) #version avec sleep
+        col += 1 # modifie la colonne pour parcourir le pateau
+    return col,lig
+
 
 ## Fonction de choix et de vérification des déplacements
 
@@ -134,6 +204,7 @@ def Dirligne (lig):#demande un déplacement sur les lignes au joueur et vérifie
             possibilite = True #le déplacement est vérifié comme possible
     return dirlig
 
+#demande les choix de déplacement du joueur et vérifie s'ils sont valides
 def Deplacementverifjoueur (lig, col, plateau):
     collision = True #variable vérifiant si le pion ne se déplace pas sur une case déjà occupée par une grenouille alliée
     while collision : #tant que la collision est vrai
@@ -155,6 +226,17 @@ def Deplacementverifjoueur (lig, col, plateau):
         else:
             collision = False #la case n'est pas occupée
     return dirlig, dircol #retourne les déplacements qui ont été vérifiés comme valides
+
+## Fonction de déplacement
+
+
+# déplace le pion en fonction du choix du joueur et supprime l'ancien emplacement
+def Deplacement(plateauPion,plateauCarte,plateauVisible):
+    col,lig = RecherchePionS(plateauPion,plateauCarte,plateauVisible) # retourne les coordonnées du pion séléctionné
+    dirlig, dircol = Deplacementverifjoueur(col, lig, plateauPion) # retourne les résultats du déplacements
+    plateauPion[lig+dirlig][col+dircol] = plateauPion[col][lig] # déplace le pion en fonction des choix
+    plateauPion[lig][col] = 0 # retire l'ancien pion
+    return plateauPion,plateauCarte,plateauVisible # retourne les plateaux modifiés
 
 ### EFFET CARTE
 
@@ -183,7 +265,7 @@ def Carte (lig, col, plateaucarte): #retourne la carte sur laquelle le pion arri
 # Brochet
 
 def Brochet (lig,col,plateauemplacement):
-    plateauemplacement[lig][col]=0
+    plateauemplacement[lig][col]=0 #supprime le pion tombé sur cette carte
 
 #Mâle
 
